1310629c3a6cb6673137a1ee57074281fe6e9bc1
vasilyev04 <vasilyev.main@gmail.com>
2024-03-29 02:02:04 +0500

    di scope fixed

--- app/src/main/java/com/vasilyev/shoppinglist/di/modules/DataModule.kt
+++ app/src/main/java/com/vasilyev/shoppinglist/di/modules/DataModule.kt
@@ -5,6 +5,7 @@ import com.vasilyev.shoppinglist.data.repository.ShopListRepositoryImpl
 import com.vasilyev.shoppinglist.data.local.LocalDataSource
 import com.vasilyev.shoppinglist.data.local.LocalDataSourceImpl
 import com.vasilyev.shoppinglist.data.local.room.AppDatabase
+import com.vasilyev.shoppinglist.di.scopes.ApplicationScope
 import com.vasilyev.shoppinglist.domain.ShopListRepository
 import dagger.Binds
 import dagger.Module
@@ -14,14 +15,17 @@ import dagger.Provides
 interface DataModule {
 
     @Binds
+    @ApplicationScope
     fun bindRepositoryImpl(impl: ShopListRepositoryImpl): ShopListRepository
 
     @Binds
+    @ApplicationScope
     fun bindLocalDataSource(impl: LocalDataSourceImpl): LocalDataSource
 
     companion object{
 
         @Provides
+        @ApplicationScope
         fun provideAppDatabase(application: Application): AppDatabase {
             return AppDatabase.getInstance(application)
         }
